fce79c26f55ba68258f94996ed88eab3268245f7
Rainrhyme <Rainrhyme@163.com>
2026-02-07 13:19:35 +0800

    Update log.h

--- module/src/main/cpp/log.h
+++ module/src/main/cpp/log.h
@@ -1,16 +1,63 @@
 //
-// Created by Perfare on 2020/7/4.
+// Logging with file output
 //
 
-#ifndef ZYGISK_IL2CPPDUMPER_LOG_H
-#define ZYGISK_IL2CPPDUMPER_LOG_H
+#ifndef ZYGISK_IRRLICHT_DUMPER_LOG_H
+#define ZYGISK_IRRLICHT_DUMPER_LOG_H
 
 #include <android/log.h>
+#include <cstdio>
+#include <ctime>
+#include <cstring>
 
-#define LOG_TAG "Perfare"
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOG_TAG "IrrlichtDumper"
 
-#endif //ZYGISK_IL2CPPDUMPER_LOG_H
+// Global log file pointer
+extern FILE *g_log_file;
+
+// Initialize log file
+inline void init_log_file(const char *game_data_dir) {
+    if (g_log_file) return;
+    
+    char log_path[256];
+    snprintf(log_path, sizeof(log_path), "%s/module_log.txt", game_data_dir);
+    g_log_file = fopen(log_path, "w");
+    
+    if (g_log_file) {
+        time_t now = time(nullptr);
+        fprintf(g_log_file, "=== IrrlichtDumper Log ===\n");
+        fprintf(g_log_file, "Time: %s\n", ctime(&now));
+        fflush(g_log_file);
+    }
+}
+
+// Close log file
+inline void close_log_file() {
+    if (g_log_file) {
+        fclose(g_log_file);
+        g_log_file = nullptr;
+    }
+}
+
+// Log to both logcat and file
+#define LOGD(...) do { \
+    __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__); \
+    if (g_log_file) { fprintf(g_log_file, "[DEBUG] " __VA_ARGS__); fprintf(g_log_file, "\n"); fflush(g_log_file); } \
+} while(0)
+
+#define LOGW(...) do { \
+    __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__); \
+    if (g_log_file) { fprintf(g_log_file, "[WARN] " __VA_ARGS__); fprintf(g_log_file, "\n"); fflush(g_log_file); } \
+} while(0)
+
+#define LOGE(...) do { \
+    __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__); \
+    if (g_log_file) { fprintf(g_log_file, "[ERROR] " __VA_ARGS__); fprintf(g_log_file, "\n"); fflush(g_log_file); } \
+} while(0)
+
+#define LOGI(...) do { \
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__); \
+    if (g_log_file) { fprintf(g_log_file, "[INFO] " __VA_ARGS__); fprintf(g_log_file, "\n"); fflush(g_log_file); } \
+} while(0)
+
+#endif //ZYGISK_IRRLICHT_DUMPER_LOG_H
